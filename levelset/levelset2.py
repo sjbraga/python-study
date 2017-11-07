a5c0b679a8a921370b578226df9948e57d2ae70a
Samira <sjbraga@projesi.com.br>
2017-11-07 09:19:16 -0200

    classical level set

---
+++ levelset/levelset2.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pylab as plt
+from matplotlib import pyplot as ppl
+import scipy.signal as signal
+import scipy.ndimage as image
+import time
+
+def gauss_kern():
+    """ Returns a normalized 2D gauss kernel array for convolutions """
+    h1 = 8
+    h2 = 8
+    x, y = np.mgrid[0:h2, 0:h1]
+    x = x-h2/2
+    y = y-h1/2
+    sigma = 10.0
+    g = np.exp( -( x**2 + y**2 ) / (2*sigma**2) )
+    return g / g.sum()
+
+Img = plt.imread("twoObj.bmp")
+Img = Img[::-1] 
+g = gauss_kern()
+Img_smooth = signal.convolve(Img,g,mode='same')
+Iy,Ix=np.gradient(Img_smooth)
+absGradI=np.sqrt(Ix**2+Iy**2)
+rows, cols = Img.shape
+
+# initial function phi - level set is a square 4 pixels
+# away from borders on each side, in 3D it looks like an empty
+# box
+c0=4
+w=4
+nrow, ncol=Img.shape
+phi=c0*np.ones((nrow,ncol))
+phi[w+1:-w-1, w+1:-w-1]=-c0
+
+# edge-stopping function
+g = 1 / (1+absGradI**2)
+
+# gradient of edge-stopping function
+gy,gx = np.gradient(g)
+
+# gradient descent step size
+dt=1
+
+# number of iterations after which we reinitialize the surface
+num_reinit=10
+
+phiOld=np.zeros((rows,cols))
+
+# number of iterations after which we reinitialize the surface
+iter=0
+
+#visual environment
+fig = ppl.gcf()
+fig.clf()
+ax1 = fig.add_subplot(1,2,1)
+ax1.imshow(Img, cmap='gray')
+
+ax2 = fig.add_subplot(1,2,2)
+ax_u = ax2.imshow(phi)
+ppl.pause(0.001)
+
+while iter<150:
+    # gradient of phi
+    gradPhiY, gradPhiX = np.gradient(phi)    
+    # magnitude of gradient of phi
+    absGradPhi=np.sqrt(gradPhiX**2+gradPhiY**2)
+    # normalized gradient of phi - eliminating singularities
+    normGradPhiX=gradPhiX/(absGradPhi+(absGradPhi==0))
+    normGradPhiY=gradPhiY/(absGradPhi+(absGradPhi==0))
+    
+    divYnormGradPhiX, divXnormGradPhiX=np.gradient(normGradPhiX)
+    divYnormGradPhiY, divXnormGradPhiY=np.gradient(normGradPhiY)
+                           
+    # curvature is the divergence of normalized gradient of phi
+    K = divXnormGradPhiX + divYnormGradPhiY
+    tmp1 = g * K * absGradPhi
+    tmp2 = g * absGradPhi
+    tmp3 = gx * gradPhiX + gy*gradPhiY
+    dPhiBydT =tmp1 + tmp2 + tmp3    
+    
+    phiOld=phi
+    # level set evolution equation    
+    phi = phi + ( dt * dPhiBydT )
+    iter=iter+1
+
+    #del ax1.collections[0]
+    ax1.contour(phi, 0, colors='r')
+    ax_u.set_data(phi)
+    fig.canvas.draw()
+    # if np.mod(iter,20)==0:
+    #     f=plt.figure()
+    #     plt.imshow(Img, cmap='gray')
+    #     CS = plt.contour(phi,0, colors='r') 
+    # plt.savefig('level_2_' + str(iter) + '.png')
