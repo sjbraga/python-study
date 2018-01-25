4ee47f9542a0726cfc1ce03f7d4a868d22c92793
Samira <sjbraga@projesi.com.br>
2018-01-25 14:18:57 -0200

    Rgb to grayscale methods

--- img_convert.py
+++ img_convert.py
@@ -1,11 +1,35 @@
+import numpy as np
 import os
 from PIL import Image
 
-path = "/media/sf_SharedWorkspace/OCT/Normal/JPG/"
-path_to_save = "/media/sf_SharedWorkspace/OCT/Normal/GRAYSCALE/"
+path = "/home/samira/mestrado/img/NORMAL/ORIGINAL/"
+path_to_save = "/home/samira/mestrado/img/NORMAL/GRAYSCALE/"
 
-listing = os.listdir(path)
+def get_info(inputPath):
+        im = Image.open(inputPath)
+        print filename
+        print im.size
+        print im.mode
 
-for filename in listing:
-    im = Image.open(path + filename).convert('L')
-    im.save(os.path.join(path_to_save, filename))
+        print "-----------------------"
+
+def load_image(intputPath):
+    img = Image.open(intputPath).convert('L')
+    img.load()
+    data = np.asarray(img, dtype="int32")
+    return data
+
+def save_image(npdata, outputPath):
+    img = Image.fromarray( np.asarray( np.clip(npdata,0,255), dtype="uint8"), "L" )
+    img.save(outputPath)
+
+if __name__ == '__main__':
+    listing = os.listdir(path)
+
+    for filename in listing:
+        outPath = os.path.join(path_to_save, filename)
+        inPath = os.path.join(path, filename)
+
+        npdata = load_image(inPath)
+        save_image(npdata, outPath)
+        get_info(outPath)
